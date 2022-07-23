89bfc1dddf60e9f9e96949a3f6911bed675642c9
Atemndobs <atemndobs@yahoo.com>
2022-07-23 16:25:41 +0200

    add kafka and zookeper

---
+++ app/Http/Controllers/Voyager/SongController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Voyager;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use TCG\Voyager\Http\Controllers\VoyagerBaseController;
+
+class SongController extends VoyagerBaseController
+{
+}
